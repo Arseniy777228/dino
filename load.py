6234348d3e2269e1fe6c1326aa940957ff1d5e14
doroshkovd <doroshkovd@yandex.ru>
2023-02-04 14:52:23 +0300

    сделал

---
+++ load.py
@@ -0,0 +1,5 @@
+import pygame
+
+dino_image = pygame.image.load("image/googe_dino.png").convert_alpha()
+earth_image = pygame.image.load("image/earth.png").convert_alpha()
+kaktus_image = pygame.image.load("image/kaktus_2.png").convert_alpha()
